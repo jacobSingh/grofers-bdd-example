f150f4a132b6a38aee73f077746373950660ca6a
Jacob Singh <jacobsingh@gmail.com>
2017-09-28 20:14:13 +0530

    blah

--- features/environment.py
+++ features/environment.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import os
 
 def set_up_selenium():
-	if (os.environ.has_key("GROFERS_BDD_LOCAL")):
+	if (os.getenv("GROFERS_BDD_LOCAL")):
 		driver = webdriver.Safari()
 		driver.set_window_size(1290, 960)
 		driver.set_window_position(0, 0)
@@ -21,8 +21,8 @@ def set_up_selenium():
 			'browserName': "chrome",
 			'version': "31",
 		}
-		username = os.environ['SAUCE_USERNAME']
-		access_key = os.environ['SAUCE_ACCESS_KEY']
+		username = os.getenv['SAUCE_USERNAME']
+		access_key = os.getenv['SAUCE_ACCESS_KEY']
 		driver = webdriver.Remote(
 			command_executor='http://{}:{}@ondemand.saucelabs.com:80/wd/hub'.format(username, access_key),
 			desired_capabilities=desired_cap)
